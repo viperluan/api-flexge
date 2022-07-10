9bd688698e57ebb2e6b18be93c5a1bd23536377f
viperluan <viperluan@gmail.com>
2022-07-10 11:08:16 -0300

    feature: Express was configured to accept json and use router.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
+const router = require('./routes/router');
 
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({'message': 'teste'})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
