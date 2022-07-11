0c4481a8d07141e5b1f57d10262df95501ed5548
viperluan <viperluan@gmail.com>
2022-07-11 13:44:40 -0300

    feature: Implemented createStudentService.

--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -5,8 +5,21 @@ studentsRoutes.get('/', (request, response) => {
   response.json({ students: [{ name: 'Maicon', age: 32 }, { name: 'Jonas', age: 21 }]});
 });
 
-studentsRoutes.post('/', (request, response) => {
-  response.status(201).send();
+studentsRoutes.post('/', async (request, response) => {
+  try {
+    const { name, age, course, school } = request.body;
+
+    if(!name || !age)
+      return response.json({ status: 'Erro', message: 'Necessário nome e idade para cadastro!'});
+
+    const createStudentService = require('../services/students/createStudentService');
+
+    const student = await createStudentService({ name, age, course, school });
+
+    response.status(201).json({ status: 'Sucesso', message: student })
+  } catch (error) {
+    response.json({ status: 'Erro', message: error.message });
+  }
 });
 
 studentsRoutes.patch('/', (request, response) => {
