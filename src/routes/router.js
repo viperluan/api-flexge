990fb5445c74e5dcc3d5635fa2e5f8bf6a78fec9
viperluan <viperluan@gmail.com>
2022-07-11 11:51:10 -0300

    feature: Import userRoutes on router.

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const studentsRoutes = require('./studentsRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/students', studentsRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
