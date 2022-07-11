8c276e449735af77086c44f1ec0a0113cf1a5a64
viperluan <viperluan@gmail.com>
2022-07-11 13:38:41 -0300

    feature: Added database flexge.

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
 
 async function createConnectionToMongoAtlas() {
-  await mongoose.connect(`mongodb+srv://luan:${password}@apis.khyd4bo.mongodb.net/?retryWrites=true&w=majority`);
+  await mongoose.connect(`mongodb+srv://luan:${password}@apis.khyd4bo.mongodb.net/flexge?retryWrites=true&w=majority`);
 }
 
 module.exports = createConnectionToMongoAtlas;
