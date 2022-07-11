eb425f86838eedb03add097bdca86cd1b80bb332
viperluan <viperluan@gmail.com>
2022-07-11 10:32:31 -0300

    feature: Added connection to mongo and created const to port.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
-const router = require('./routes/router');
 
+const createConnectionDB = require('./db/connection');
+createConnectionDB().then(() => console.log('Successful connected to mongo atlas.'))
+  .catch(error => console.log('Failed to connect on mongo atlas.'));
+
+const EXPRESS_PORT = process.env.PORT;
 const express = require('express');
 const app = express();
+const router = require('./routes/router');
 
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server started on port ${process.env.PORT}`);
-});
+app.listen(EXPRESS_PORT, () => console.log(`Server started on port: ${EXPRESS_PORT}`));
