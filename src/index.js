17a8914b102674b18bfc1eca6ae4ad2d49faa01b
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-18 02:28:54 -0300

    feature: Implemented cors lib on server.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const createConnectionDB = require('./db/connection');
 createConnectionDB().then(() => console.log('Successful connected to mongo atlas.'))
   .catch(error => console.log('Failed to connect on mongo atlas.'));
 
+const cors = require('cors');
+
 const EXPRESS_PORT = process.env.PORT;
 const express = require('express');
 const app = express();
 const router = require('./routes/router');
 const authenticationMiddleware = require('./middlewares/authentication');
 
+app.use(cors());
 app.use(authenticationMiddleware);
 
 app.use(express.json());
